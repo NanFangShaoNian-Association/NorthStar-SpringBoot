bfbf4f502ce19ed565e489d3f88b85de26c0956a
AtnibamAitay <atnibamaitay@foxmail.com>
2022-11-21 15:49:22 +0800

    Add mapstruct

--- src/main/java/com/nfsn/PetPlatformMain.java
+++ src/main/java/com/nfsn/PetPlatformMain.java
@@ -15,6 +15,5 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PetPlatformMain {
     public static void main(String[] args) {
         SpringApplication.run(PetPlatformMain.class,args);
-        System.out.println("test");
     }
 }
